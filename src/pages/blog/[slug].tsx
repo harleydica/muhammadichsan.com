44d75c21b1c02fff6516690685c1afb24bd415f6
Muhammad Ichsanul Fadhil <69036480+ichsanputr@users.noreply.github.com>
2022-07-02 00:50:32 -0700

    Update [slug].tsx

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,3 +1,4 @@
+
 import BackToTop from '@/components/atoms/BackToTop'
 import CustomImage from '@/components/atoms/CustomImage'
 import EditButton from '@/components/mollecules/EditButton'
@@ -8,19 +9,19 @@ import Layout from '@/components/templates/Layout'
 
 import { Blogs } from '@/data/blog/blog.type'
 import { getBlog, getBlogBySlug } from '@/helpers/getBlog'
-// import { isProd } from '@/libs/constants/environmentState'
+import { isProd } from '@/libs/constants/environmentState'
 import dateFormat, { dateStringToISO } from '@/libs/dateFormat'
 import { getMetaDataBlog } from '@/libs/metaData'
 import { twclsx } from '@/libs/twclsx'
-// import umamiClient from '@/libs/umamiClient'
+import umamiClient from '@/libs/umamiClient'
 
 import { LayoutProps } from 'framer-motion'
 import { GetStaticPaths, GetStaticPathsResult, GetStaticProps, NextPage } from 'next'
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
 import { ParsedUrlQuery } from 'querystring'
-// import { useEffect, useState } from 'react'
-import { HiOutlineCalendar, HiOutlineClock } from 'react-icons/hi'
+import { useEffect, useState } from 'react'
+import { HiOutlineCalendar, HiOutlineClock, HiOutlineEye } from 'react-icons/hi'
 import readingTime from 'reading-time'
 import rehypeSlug from 'rehype-slug'
 
@@ -40,6 +41,7 @@ interface HTTP {
 }
 
 const BlogPost: NextPage<BlogPostProps> = ({ header, mdxSource }) => {
+  const [postViews, setPostViews] = useState<number>(0)
 
   const metaData = getMetaDataBlog({
     ...header,
@@ -52,6 +54,22 @@ const BlogPost: NextPage<BlogPostProps> = ({ header, mdxSource }) => {
     month: 'short',
     year: 'numeric'
   }
+
+  useEffect(() => {
+    ;(async () => {
+      if (isProd) {
+        try {
+          const response = await umamiClient.get<HTTP>('/api/umami/blogviews?slug=' + header.slug)
+
+          setPostViews(response.data.data ?? 0)
+        } catch (error) {
+          console.info('Could not retrieve page views')
+        }
+      } else {
+        setPostViews(0)
+      }
+    })()
+  }, [header.slug])
 
   return (
     <Layout {...(metaData as LayoutProps)}>
@@ -66,6 +84,11 @@ const BlogPost: NextPage<BlogPostProps> = ({ header, mdxSource }) => {
               <div className={twclsx('flex items-center', 'gap-2', 'text-sm md:text-base')}>
                 <HiOutlineClock className={twclsx('text-lg')} />
                 <p>{header.est_read}</p>
+              </div>
+
+              <div className={twclsx('flex items-center', 'gap-2', 'text-sm md:text-base')}>
+                <HiOutlineEye className={twclsx('text-lg')} />
+                {postViews > 0 ? <p>{postViews} views</p> : <p>—</p>}
               </div>
             </div>
             <div className={twclsx('flex items-center', 'gap-2')}>
