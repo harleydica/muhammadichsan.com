b94f79393c8082cf7f6b7fadcabc4dace271a7f2
Muhammad Ichsanul Fadhil <69036480+ichsanputr@users.noreply.github.com>
2022-07-02 00:56:14 -0700

    Update umamiClient.ts

--- src/libs/umamiClient.ts
+++ src/libs/umamiClient.ts
@@ -1,11 +1,11 @@
 import Axios from 'axios'
 
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://rizkicitra.dev'
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL
 
 const headers = { 'Content-Type': 'application/json' }
 
 const umamiClient = Axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : SITE_URL,
+  baseURL: SITE_URL,
   headers
 })
 
