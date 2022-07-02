634719a523be94fbc1685b5ae76846fea3d1ea29
ichsanputr <githubichsan@gmail.com>
2022-07-02 15:06:57 +0800

    change link social media

--- src/libs/constants/social.ts
+++ src/libs/constants/social.ts
@@ -1,19 +1,19 @@
 const SOCIAL = [
   {
     title: 'GitHub',
-    href: 'https://github.com/rizkimcitra'
+    href: 'https://github.com/ichsanputr'
   },
   {
     title: 'Telegram',
-    href: 'https://t.me/rizkimcitra'
+    href: 'https://t.me/ichsanputr'
   },
   {
     title: 'Facebook',
-    href: 'https://facebook.com/rizkimcitraa'
+    href: 'https://facebook.com/ichsanputr'
   },
   {
     title: 'Email',
-    href: 'mailto: rmaulana.citra@gmail.com?subject='
+    href: 'mailto: ichsanfadhil67@gmail.com?subject='
   }
 ]
 
