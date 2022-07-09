97ea848b5c652a526de66c61a75a4f6a1838c2d1
Muhammad Ichsanul Fadhil <69036480+ichsanputr@users.noreply.github.com>
2022-07-08 20:41:06 -0700

    Update certificate.ts

--- src/libs/constants/certificate.ts
+++ src/libs/constants/certificate.ts
@@ -2,6 +2,10 @@ type Albums = Array<{ title: string; src: string }>
 
 const ALBUMS: Albums = [
   {
+    title: '(Certificate) 2nd Place in the AWS Static Web C3 Competition at the Provincial Level DIYogyakarta',
+    src: `https://ik.imagekit.io/uvma3bkbp/brave_39aufw60hG_AoDdB4Q1j.png`
+  },
+  {
     title: '2nd Place in the AWS Static Web C3 Competition at the Provincial Level DIYogyakarta',
     src: `https://ik.imagekit.io/uvma3bkbp/lomba_aws_GO3w5S6jj.jpg`
   },
