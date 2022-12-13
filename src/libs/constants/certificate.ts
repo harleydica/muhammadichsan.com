2edbefb7baf1a0eba05ae5d4e46c9248af17f5ac
Muhammad Ichsanul Fadhil <69036480+ichsanputr@users.noreply.github.com>
2022-12-13 18:26:53 +0800

    a

--- src/libs/constants/certificate.ts
+++ src/libs/constants/certificate.ts
@@ -6,20 +6,12 @@ const ALBUMS: Albums = [
     src: `https://ik.imagekit.io/uvma3bkbp/brave_39aufw60hG_AoDdB4Q1j.png`
   },
   {
-    title: '2nd Place in the AWS Static Web C3 Competition at the Provincial Level DIYogyakarta - Muhammad Ichsanul Fadhil',
-    src: `https://ik.imagekit.io/uvma3bkbp/lomba_aws_GO3w5S6jj.jpg`
-  },
-  {
     title: '(Certificate) 1st Place in BDA Programming Competition at National Level - Muhammad Ichsanul Fadhil',
     src: `https://ik.imagekit.io/uvma3bkbp/lomba_bda_Sj4mTZxTy.png`
   },
   {
     title: '1st Place in BDA Programming Competition at National Level - Muhammad Ichsanul Fadhil',
     src: `https://ik.imagekit.io/uvma3bkbp/lomba_bda_panggung_cpZu8UWgh.jpg`
-  },
-  {
-    title: '3rd Place FIKSI (Indonesian Student Innovation and Entrepreneurship Festival) at Provincial Level ',
-    src: `https://ik.imagekit.io/uvma3bkbp/WhatsApp_Image_2022-08-26_at_10.01.23_lpbzUyLvy.jpeg`
   },
 ]
 
