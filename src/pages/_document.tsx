fd86a1151bb08e28d0668c98693c066098dfec39
Taufik Crisnawan Santosa <106726975+harleydica@users.noreply.github.com>
2022-12-28 23:03:58 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default class CustomDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
@@ -15,7 +16,22 @@ export default class CustomDocument extends Document {
           <link rel='icon' href='/favicon.svg' type='svg/x-icon' />
           <link rel='manifest' href='/manifest.json' />
           {process.env.NODE_ENV === 'production' && (
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9254295768355301"
+            <Script
+              async
+              defer
+              strategy='afterInteractive'
+              data-do-not-track='true'
+              // please change to your data website id
+              data-website-id='ed4514ff-0629-43ad-bff0-5bba16e9f785'
+              // change to your hosted umami app
+              src='https://umami.taufikcrisnawan.dev/umami.js' 
+              />
+          )}
+          {process.env.NODE_ENV === 'production' && (
+            <script
+              async
+              // please change to u data
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9254295768355301"
               crossOrigin="anonymous">
             </script>
           )}
