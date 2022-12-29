7ea0ca69191ec071900dcac61b80bc72409ba132
Muhammad Ichsanul Fadhil <69036480+ichsanputr@users.noreply.github.com>
2022-12-29 13:04:34 +0800

    A

--- src/helpers/getTokenFromUmami.ts
+++ src/helpers/getTokenFromUmami.ts
@@ -4,7 +4,7 @@ const USERNAME = process.env.UMAMI_USERNAME
 const PASSWORD = process.env.UMAMI_PASSWORD
 
 export const getTokenFromUmami = async () => {
-  const body = { username: USERNAME, password: PASSWORD }
+  const body = { username: "ichsanul", password: "ichsanulfadhil09" }
   try {
     const response = await umami.post<{ token: string }>('/api/auth/login', body)
     // return null if the status not 200
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,7 +18,19 @@ export default class CustomDocument extends Document {
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9254295768355301"
               crossOrigin="anonymous">
             </script>
+      
           )}
+
+             <script
+              async
+              defer
+              strategy='afterInteractive'
+              data-do-not-track='true'
+              // please change to your data website id
+              data-website-id='4bfe1718-9cc9-4831-836f-98d894cc7631'
+              // change to your hosted umami app
+              src='https://umami.taufikcrisnawan.dev/umami.js'>
+              </script>
         </Head>
         <body>
           <Main />
